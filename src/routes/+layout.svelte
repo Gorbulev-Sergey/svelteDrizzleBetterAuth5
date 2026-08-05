5913cd36c8096fd7a87c412906fba8d4cff4f950
Горбулёв Сергей <gorbulevsv@yandex.ru>
2026-08-05 11:48:19 +0300

    Favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import { resolve } from '$app/paths';
+	import favicon from '$lib/assets/favicon.svg';
+
 	let { children } = $props();
 </script>
+
+<svelte:head>
+	<link rel="icon" href={favicon} />
+	<title>Drizzle BetterAuth проект</title>
+</svelte:head>
 
 <div class="sticky-top w-100 bg-light">
 	<div class="d-flex align-items-center px-lg-3 py-2">
